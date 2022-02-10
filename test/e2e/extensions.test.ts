9e7fcfae498b5c0e5f6b457ec4482d3777c93b5d
Joe Previte <jjprevite@gmail.com>
2022-02-10 16:43:49 -0700

    feat: add --cert e2e extension test

--- test/e2e/extensions.test.ts
+++ test/e2e/extensions.test.ts
@@ -1,12 +1,21 @@
 import { describe, test } from "./baseFixture"
 
-describe("Extensions", true, () => {
+function runTestExtensionTests() {
   // This will only work if the test extension is loaded into code-server.
   test("should have access to VSCODE_PROXY_URI", async ({ codeServerPage }) => {
     const address = await codeServerPage.address()
 
     await codeServerPage.executeCommandViaMenus("code-server: Get proxy URI")
 
-    await codeServerPage.page.waitForSelector(`text=${address}/proxy/{port}`)
+  // Click span:has-text("https://localhost:57989/proxy/{{port}}")
+    await codeServerPage.page.waitForSelector(`span:has-text("${address}/proxy/{{port}}")`)
   })
+}
+
+describe("Extensions", true, [], () => {
+  runTestExtensionTests()
+})
+
+describe("Extensions with --cert", true, ["--cert"], () => {
+  runTestExtensionTests()
 })
